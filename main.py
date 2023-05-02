b3d9f9eb0e4be0a88f7665ec785add70ad622da6
Ameer Abdullah <ameer.abdullah@gmail.com>
2023-05-02 19:34:20 +0500

    Mod Implementation

--- main.py
+++ main.py
@@ -10,9 +10,13 @@ def multiply(a,b):
 def divide(a,b):
     return a/b
 
+def mod(a,b):
+    return a%b
+
 
 if __name__ == '__main__':
     print(add(2,3))
     print(subtract(5,3))
     print(multiply(3,4))
     print(divide(6,3))
+    print(mod(5,3))
