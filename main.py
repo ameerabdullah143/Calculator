bad00c5e661a26bed574d7248b6e54cbbe907a74
Ameer Abdullah <ameer.abdullah@gmail.com>
2023-05-02 19:33:01 +0500

    Divide Implementation

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ def subtract(a,b):
 def multiply(a,b):
     return a*b
 
+def divide(a,b):
+    return a/b
+
 
 if __name__ == '__main__':
     print(add(2,3))
     print(subtract(5,3))
     print(multiply(3,4))
+    print(divide(6,3))
